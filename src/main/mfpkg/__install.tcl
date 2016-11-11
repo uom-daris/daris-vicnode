df165b4702c8214736baea405f024dbe1e41a751
Wilson Liu <wliu5@unimelb.edu.au>
2016-11-11 13:45:28 +1100

    added animal generic method.

--- src/main/mfpkg/__install.tcl
+++ src/main/mfpkg/__install.tcl
@@ -92,6 +92,9 @@ create_method_generic ${DOC_NS} $methodAction $fillInMethods
 source method-human-generic.tcl
 create_method_human_generic ${DOC_NS} $methodAction $fillInMethods
 
+source method-animal-generic.tcl
+create_method_animal_generic ${DOC_NS} $methodAction $fillInMethods
+
 source method-animal-multimode.tcl
 create_method_animal_multimode ${DOC_NS} $methodAction $fillInMethods
 
@@ -100,6 +103,7 @@ create_method_mds_femur ${DOC_NS} $methodAction $fillInMethods
 
 source method-human-mammography.tcl
 create_method_human_mammography ${DOC_NS} $methodAction $fillInMethods
+
 
 # ============================================================================
 # Add plugin module
